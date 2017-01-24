79d4d30c2d5459ab597a3e41d42f298c25f7a1f3
BoenYang <yang0927mail@gmail.com>
2017-01-24 14:33:54 +0800

    init repo

---
+++ Assets/BaseFrameWork/EnitySystem/EnityMng.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class EnityMng : MonoBehaviour {
+
+    protected Dictionary<ulong, Enity> enityDict = new Dictionary<ulong, Enity>();
+
+    protected ulong currentEnityId = 0;
+
+    protected ulong GetUniqueEnityId()
+    {
+        currentEnityId++;
+        return currentEnityId;
+    }
+}
